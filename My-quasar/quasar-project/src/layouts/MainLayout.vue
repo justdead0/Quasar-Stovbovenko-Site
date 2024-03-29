44406588b5a4df4259ffcb4747c42b41be8885ef
justdead0 <stasstas2034@gmail.com>
2024-03-30 02:08:02 +0300

    Quasar Site Tovbovenko

---
+++ My-quasar/quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,163 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+    
+      
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+      <div class="text-h3"> Мой список дел </div>
+          <div class="text-subtitlel">{{ todaysDate }}</div>
+        </div>
+        <q-img 
+          src="../assets/Rei.jpg"
+          class="header-image absolute-top"/>
+    </q-header>
+
+    <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 185px); margin-top: 185px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item 
+              to="/"
+              exact
+            clickable 
+            v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Список дел
+              </q-item-section>
+            </q-item>
+
+            <q-item 
+             to="/help"
+             exact 
+             clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+                Помощь
+              </q-item-section>
+            </q-item>
+
+           
+
+            
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="../assets/space.jpg" style="height: 185px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="../assets/galaxy.jpg">
+            </q-avatar>
+            <div class="text-weight-bold">Stanislav Stovbovenko</div>
+            <div>@stanislav__stovbovenko</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+           <router-view />
+      </keep-alive>
+   
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  },
+  computed: {
+    todaysDate() {
+      let timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd D MMMM YYYY')
+    }
+  }
+}
+</script>
+
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+const leftDrawerOpen = ref(false)
+
+
+<style lang="scss">
+  .header-image{
+    height:100%;
+    z-index: -1;
+    opacity: 0.2;
+    filter: grayscale(100%)
+
+      }
+</style>
